f5aa958329c3ee08be8c90cfd79f804ab49edcc4
Muddisett <muddisettypremkumar562@gmail.com>
2023-12-14 13:55:53 +0530

    password removes

--- src/com/jsp/User.java
+++ src/com/jsp/User.java
@@ -4,13 +4,8 @@ public class User {
 private int userId;
 private String userName;
 private String userEmail;
-private String userpassword;
-public String getUserpassword() {
-	return userpassword;
-}
-public void setUserpassword(String userpassword) {
-	this.userpassword = userpassword;
-}
+
+
 public int getUserId() {
 	return userId;
 }
